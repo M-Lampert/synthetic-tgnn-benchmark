16be3d25403e4f51db8ffdb396fcd5c1769145c7
Moritz Lampert <moritz@lampert.ai>
2024-09-06 11:23:19 +0000

    bash script

---
+++ run_experiments.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GPU="0"
+datasets=("periodic" "burst" "bipartite" "triadic")
+models=("JODIE") # "DyRep" "TGAT" "TGN" "CAWN" "TCL" "GraphMixer" "DyGFormer")
+shuffle_orders=("" "--shuffle_order")
+arange_timestamps=("" "--arange_timestamp")
+
+for dataset in "${datasets[@]}"
+do
+    for model in "${models[@]}"
+    do
+        for shuffle_order in "${shuffle_orders[@]}"
+        do
+            for arange_timestamp in "${arange_timestamps[@]}"
+            do
+                python train_link_prediction.py --dataset_name $dataset --model_name $model --patience 5 --load_best_configs --num_runs 5 --gpu $GPU --negative_sample_strategy historical $shuffle_order $arange_timestamp
+            done
+        done
+    done
+done
+
+for dataset in "${datasets[@]}"
+do
+    for shuffle_order in "${shuffle_orders[@]}"
+    do
+        for arange_timestamp in "${arange_timestamps[@]}"
+        do
+            python evaluate_link_prediction.py --dataset_name $dataset --model_name EdgeBank --patience 5 --load_best_configs --num_runs 5 --gpu $GPU --negative_sample_strategy historical $shuffle_order $arange_timestamp
+        done
+    done
+done
